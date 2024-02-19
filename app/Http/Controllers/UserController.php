5234e145a9a896735a7507eee449ae507c46ecfa
Ilham Budiawan Sitorus <60646465+Ilham808@users.noreply.github.com>
2024-02-19 17:02:32 +0700

    feat: authentication login dan register

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class UserController extends Controller
+{
+    public function login(LoginUserRequest $request): JsonResponse {
+
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            throw new HttpResponseException(response()->json([
+                'success' => false,
+                'error' => [
+                    'message' => 'Email atau password salah'
+                ]
+            ], 401));
+        }
+
+        $user->token = Str::uuid()->toString();
+
+        $user->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Berhasil',
+            'data' => new UserResource($user)
+        ]);
+
+    }
+
+    public function register(RegisterUserRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+        $user->token = Str::uuid()->toString();
+        $user->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Register Berhasil',
+            'data' => new UserResource($user)
+        ], 201);
+    }
+
+    public function profile(Request $request): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => new UserResource(Auth::user())
+        ]);
+    }
+}
